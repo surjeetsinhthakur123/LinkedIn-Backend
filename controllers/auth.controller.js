b7582e9946558e95aee2c8e82993cbb2a276443f
Surjeetsinh Nandkumar Thakur <tsurjeet143@gmail.com>
2025-10-30 07:54:00 +0530

    Refactor signup and login methods for clarity

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { sendWelcomeEmail } from "../emails/emailHandlers.js";
 
 export const signup = async (req, res) => {
     try {
@@ -10,7 +11,7 @@ export const signup = async (req, res) => {
             return res.status(400).json({ message: "All fields are required" });
         }
 
-        const existingEmail = await User.findOne({ email });
+        const existingEmail = await User.findOne({ email })
         if (existingEmail) {
             return res.status(400).json({ message: "Email already exists" });
         }
@@ -21,37 +22,43 @@ export const signup = async (req, res) => {
         }
 
         if (password.length < 6) {
-            return res.status(400).json({ message: "Password must be at least 6 characters" });
+            return res.status(400).json({ message: "Password must be atleast 6 character" });
         }
 
         const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
+        const hashedPassword = await bcrypt.hash(password, salt)
 
         const user = new User({
             name,
             username,
             email,
             password: hashedPassword
-        });
-
+        })
         await user.save();
 
-        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "3d" });
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "3d" })
 
         res.cookie("jwt-linkedIn", token, {
-            httpOnly: true, // Prevent XSS attacks
-            maxAge: 3 * 24 * 60 * 60 * 1000, // 3 days
-            sameSite: "strict", // Prevent CSRF attacks
-            secure: process.env.NODE_ENV === "production" // HTTPS-only in production
-        });
+            httpOnly: true, //prevent XSS attack
+            maxAge: 3 * 24 * 60 * 60 * 1000,  //milliseconds
+            sameSite: "strict", //prevent CSRF attack
+            secure: process.env.NODE_ENV === "production", //prevents man in the middle attacks
+        })
 
         res.status(200).json({ message: "User registered successfully" });
 
+        const profileUrl = process.env.CLIENT_URL + "/profile/" + user.username;
+        try {
+            await sendWelcomeEmail(user.email, user.name, profileUrl)
+        } catch (emailError) {
+            console.log("Error sending welcome Email ", emailError)
+        }
+
     } catch (error) {
-        console.error("Error in signup:", error.message);
+        console.log("Error in signup: ", error.message);
         res.status(500).json({ message: "Internal server error" });
     }
-};
+}
 
 export const login = async (req, res) => {
     try {
@@ -68,31 +75,29 @@ export const login = async (req, res) => {
         }
 
         const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "3d" });
-
-        res.cookie("jwt-linkedIn", token, {
+        await res.cookie("jwt-linkedIn", token, {
             httpOnly: true,
             maxAge: 3 * 24 * 60 * 60 * 1000,
             sameSite: "strict",
             secure: process.env.NODE_ENV === "production"
-        });
-
+        })
         res.status(200).json({ message: "Logged in successfully" });
     } catch (error) {
-        console.error("Error in login controller:", error);
+        console.log("Error in login controller ", error);
         res.status(500).json({ message: "Internal server error" });
     }
-};
+}
 
 export const logout = async (req, res) => {
     res.clearCookie("jwt-linkedIn");
     res.status(200).json({ message: "Logged out successfully" });
-};
+}
 
 export const getCurrentUser = async (req, res) => {
-    try {
-        res.json(req.user);
-    } catch (error) {
-        console.error("Error in getCurrentUser controller:", error);
-        res.status(500).json({ message: "Internal server error" });
-    }
+	try {
+		res.json(req.user);
+	} catch (error) {
+		console.error("Error in getCurrentUser controller:", error);
+		res.status(500).json({ message: "Internal Server error" });
+	}
 };
