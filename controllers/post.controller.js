2913e6277ce81e71c57d1f83f0797a1a76d26e44
Surjeetsinh Nandkumar Thakur <tsurjeet143@gmail.com>
2025-10-30 07:54:42 +0530

    Update post.controller.js

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,145 +1,145 @@
 import cloudinary from "../lib/cloudinary.js";
-import Post from "../models/post.model.js";
+import Post from "../models/post.model.js"
 import Notification from "../models/notification.model.js";
+import { sendCommentNotificationEmail } from "../emails/emailHandlers.js";
 
-export const getFeedPosts = async (req, res) => {
-  try {
-    const posts = await Post.find({
-      author: { $in: [...req.user.connections, req.user._id] },
-    })
-      .populate("author", "name username profilePicture headline")
-      .populate("comments.user", "name profilePicture")
-      .sort({ createdAt: -1 });
-
-    res.status(200).json(posts);
-  } catch (error) {
-    console.log("Error in getFeedPosts controller:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-};
-
-export const createPost = async (req, res) => {
-  try {
-    const { content, image } = req.body;
-    let newPost;
-
-    if (image) {
-      const imgResult = await cloudinary.uploader.upload(image);
-      newPost = new Post({
-        author: req.user._id,
-        content,
-        image: imgResult.secure_url,
-      });
-    } else {
-      newPost = new Post({
-        author: req.user._id,
-        content,
-      });
+export const getFeedPosts=async(req,res)=>{
+    try {
+        const posts = await Post.find({ author: { $in: [...req.user.connections, req.user._id] } })
+        .populate("author","name username profilePicture headline")
+        .populate("comments.user", "name profilePicture")
+        .sort({createdAt:-1});
+        res.status(200).json(posts);
+    } catch (error) {
+        console.log("Error in getFeedPosts controller: ",error);
+        res.status(500).json({message:"Internal server error"});
     }
+}
 
-    await newPost.save();
-    res.status(200).json(newPost);
-  } catch (error) {
-    console.log("Error in createPost controller:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-};
-
-export const deletePost = async (req, res) => {
-  try {
-    const postId = req.params.id;
-    const userId = req.user._id;
-
-    const post = await Post.findById(postId);
-    if (!post) return res.status(404).json({ message: "Post not found" });
-
-    if (post.author.toString() !== userId.toString()) {
-      return res.status(403).json({ message: "You are not authorized to delete this post" });
+export const createPost=async(req,res)=>{
+    try {
+        const {content, image}=req.body;
+        let newPost;
+        if(image){
+            const imgResult=await cloudinary.uploader.upload(image);
+            newPost=new Post({
+                author:req.user._id,
+                content,
+                image:imgResult.secure_url,
+            })
+        }else{
+            newPost=new Post({
+                author:req.user._id,
+                content
+            })
+        }
+        await newPost.save();
+        res.status(200).json(newPost);
+    } catch (error) {
+        console.log("Error in createPost controller: ",error);
+        res.status(500).json({message:"Internal server error"});
     }
+}
 
-    if (post.image) {
-      await cloudinary.uploader.destroy(post.image.split("/").pop().split(".")[0]);
+export const deletePost=async(req,res)=>{
+    try {
+        const postId=req.params.id;
+        const userId=req.user._id;
+
+        const post=await Post.findById(postId);
+
+        if(!post){
+            res.status(404).json({message:"Post not found"});
+        }
+
+        if(post.author.toString() !== userId.toString()){
+            return res.status(403).json({message:"You are not authorized to delete the post"})
+        }
+
+        if(post.image){
+            await cloudinary.uploader.destroy(post.image.split("/").pop().split(".")[0]);
+        }
+        await Post.findByIdAndDelete(postId);
+        res.status(200).json({message:"Post deleted successfully"});
+    } catch (error) {
+        console.log("Error in deletePost controller: ",error);
+        res.status(500).json({message:"Internal server error"});
     }
+}
 
-    await Post.findByIdAndDelete(postId);
-    res.status(200).json({ message: "Post deleted successfully" });
-  } catch (error) {
-    console.log("Error in deletePost controller:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-};
+export const getPostById=async(req,res)=>{
+    try {
+        const postId=req.params.id;
+        const post=await Post.findById(postId)
+        .populate("author","name username profilePicture headline")
+        .populate("comments.user","name profilePicture username headline");
 
-export const getPostById = async (req, res) => {
-  try {
-    const postId = req.params.id;
-    const post = await Post.findById(postId)
-      .populate("author", "name username profilePicture headline")
-      .populate("comments.user", "name profilePicture username headline");
-
-    res.status(200).json(post);
-  } catch (error) {
-    console.log("Error in getPostById controller:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-};
-
-export const createComment = async (req, res) => {
-  try {
-    const postId = req.params.id;
-    const { content } = req.body;
-
-    const post = await Post.findByIdAndUpdate(
-      postId,
-      { $push: { comments: { user: req.user._id, content } } },
-      { new: true }
-    ).populate("author", "name email username headline profilePicture");
-
-    if (post.author._id.toString() !== req.user._id.toString()) {
-      const newNotification = new Notification({
-        recipient: post.author,
-        type: "comment",
-        relatedUser: req.user._id,
-        relatedPost: postId,
-      });
-      await newNotification.save();
+        res.status(200).json(post);
+    } catch (error) {
+        console.log("Error in getPostById controller: ", error);
+        res.status(500).json({message:"Internal server error"});
     }
+}
 
-    res.status(200).json(post);
-  } catch (error) {
-    console.log("Error in createComment controller:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-};
+export const createComment=async(req,res)=>{
+    try {
+        const postId=req.params.id;
+        const {content} =req.body;
 
-export const likePost = async (req, res) => {
-  try {
-    const postId = req.params.id;
-    const post = await Post.findById(postId);
-    const userId = req.user._id;
+        const post=await Post.findByIdAndUpdate(postId,{
+            $push:{comments:{user:req.user._id,content}},
 
-    if (post.likes.includes(userId)) {
-      post.likes = post.likes.filter((id) => id.toString() !== userId.toString());
-    } else {
-      post.likes.push(userId);
+        },{new:true}).populate("author","name email username headline profilePicture");
+
+        if(post.author._id.toString() !== req.user._id.toString()){
+            const newNotification=new Notification({
+                recipient:post.author,
+                type:"comment",
+                relatedUser:req.user._id,
+                relatedPost:postId
+            });
+            await newNotification.save();
+            try {
+                const postUrl=process.env.CLIENT_URL+"/post/"+postId;
+                await sendCommentNotificationEmail(post.author.email,post.author.name,req.user.name,postUrl,content)
+            } catch (error) {
+                console.log("Error in sending comment notification email: ",error)
+            }
+            res.status(200).json(post)
+        }
+    } catch (error) {
+        console.log("Error in createComment controller: ",error);
+        res.status(500).json({message:"Internal server error"});
     }
+}
 
-    if (post.author.toString() !== userId.toString()) {
-      const newNotification = new Notification({
-        recipient: post.author,
-        type: "like",
-        relatedUser: userId,
-        relatedPost: postId,
-      });
-      await newNotification.save();
+export const likePost=async(req,res)=>{
+    try {
+        const postId=req.params.id;
+        const post=await Post.findById(postId);
+        const userId=req.user._id;
+        if(post.likes.includes(userId)){
+            post.likes=post.likes.filter(id=>id.toString()!==userId.toString())
+        }else{
+            post.likes.push(userId);
+        }
+        if(post.author.toString()!==userId.toString()){
+            const newNotification=new Notification({
+                recipient:post.author,
+                type:"like",
+                relatedUser:userId,
+                relatedPost:postId
+            });
+            await newNotification.save();
+        }
+
+        await post.save();
+        res.status(200).json(post);
+    } catch (error) {
+        console.log("Error in likePost controller: ", error);
+        res.status(500).json({message:"Internal server error"});
     }
-
-    await post.save();
-    res.status(200).json(post);
-  } catch (error) {
-    console.log("Error in likePost controller:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-};
+}
 
 export const updatePost = async (req, res) => {
   try {
@@ -173,7 +173,7 @@ export const updatePost = async (req, res) => {
       post.image = uploadResult.secure_url;
     }
 
-    post.isEdited = true;
+    post.isEdited = true; 
     await post.save();
 
     res.status(200).json({ message: "Post updated successfully", post });
@@ -182,3 +182,4 @@ export const updatePost = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
